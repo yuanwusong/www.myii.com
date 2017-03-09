8247f17665a9901a563ed7b4d7777a2f42f76323
yuanwu <2402423960@qq.com>
2017-03-09 08:15:21 +0800

    add

--- controllers/SiteController.php
+++ controllers/SiteController.php
@@ -8,9 +8,29 @@ use yii\web\Controller;
 use yii\filters\VerbFilter;
 use app\models\LoginForm;
 use app\models\ContactForm;
+use app\models\EntryForm;
 
 class SiteController extends Controller
 {
+	public function actionEntry()
+    {
+        $model = new EntryForm();
+
+        if ($model->load(Yii::$app->request->post()) && $model->validate()) {
+            // 验证 $model 收到的数据
+ 
+            // 做些有意义的事 ...
+ 
+            return $this->render('entry-confirm', ['model' => $model]);
+        } else {
+            // 无论是初始化显示还是数据验证错误
+            return $this->render('entry', ['model' => $model]);
+        }
+    }
+
+	public function actionSay($message = "Hello World!") {
+		return $this->render('say', ["message" => $message]);
+	}
     public function behaviors()
     {
         return [
